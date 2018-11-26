d38feead4419dbfb7784485c3d63f2b133d7bab9
Christian Bilke <cgbilke1@gmail.com>
2018-11-26 14:21:36 -0600

    Restoration

--- src/OrderSystem/Menu RD 1.py
+++ src/OrderSystem/Menu RD 1.py
@@ -1,6 +1,29 @@
-import string
+mport string
+import json
+
 from collections import namedtuple
-# Not the Final menu
+
+#Not the Final menu
+#Class definition for menu item incase swtich is needed for JSON
+#idC = 1;
+#class menu_item:
+    #id = -1
+    #name = 'null'
+    #description = 'null'
+    #price = 'null'
+    #def __init__(self,name,description,price):
+        #global idC;
+        #self.id = idC
+        #idC += 1
+        #self.name=name
+        #self.description=description
+        #self.price=price
+#menuCl = []
+#menuCl.append(menu_item('Hamburger' ,  'Juicy hamburger made with prime beef',   '$5.00'))
+#menuCl.append(menu_item('Chilli Dog','Chilli dog served with chilli and cheese','$3.25'))
+#menuCl.append(menu_item('Nachos','Nachos served with chilli, chesse and jalapenos', '$3.50'))
+#menuCl.append(menu_item('Tacos','Your choice of hard or so˙˙t shelled Taco, toppings included' ,'$5.25'))
+#menuCl.append(menu_item('Pizza','Large hand tossed pizza', '$5.00'))
 
 Menu_Item = namedtuple('Menu_Item', ['ID','name','description','price'])
 menu = []
@@ -16,14 +39,11 @@ for entry in menu:
     price = getattr(entry,'price').ljust(25)
     name = getattr(entry, 'name').ljust(15)
     print ('{0}{1}{2}{3}'.format(ID, name , description , price))
-
-def GetAvailableItems():
-# will work with inventory
-    print (menu)
-
-def FindMenuItem(search_text: string):
-# Needs work
-    string = menu.name
-    string.find(name)
-
-
+    def GetAvailableItems():
+        return(json.dumps(menu))
+    def FindMenuItem(searchString):
+        print(searchString)
+    for entry in menu:
+        if entry[1]==searchString:
+            return(json.dumps(entry))
+    return 'menu item not found'
