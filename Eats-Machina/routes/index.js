7fa137440b2fdb82571491bd0a12ccf63b09dd21
Joseph <Josephafordjr@gmail.com>
2018-12-07 12:13:48 -0600

    added missing filex and exes

---
+++ Eats-Machina/routes/index.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    //console.log(req.user);
+    res.render('index', { user: req.session.user, title: 'Eats-Machina' });
+});
+
+module.exports = router;
